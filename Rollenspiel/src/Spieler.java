6a6aa7f701d2dd52dc4a3adc0c15b7b3f6455532
lilVirno <virnich.lukas@gmail.com>
2026-02-09 10:10:04 +0100

    Spieler Klasse erstellt; attribute name und Punktekonto hinzugefügt; getter und setter sowie konstruktor hinzugefügt

--- Rollenspiel/src/Spieler.java
+++ Rollenspiel/src/Spieler.java
@@ -1,2 +1,28 @@
 public class Spieler {
+
+    private String name;
+
+    private int Punktekonto;
+
+
+    public Spieler (String name, int Punkte) {
+        setName(name);
+        setPunktekonto(Punkte);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPunktekonto() {
+        return Punktekonto;
+    }
+
+    public void setPunktekonto(int punktekonto) {
+        Punktekonto = punktekonto;
+    }
 }
