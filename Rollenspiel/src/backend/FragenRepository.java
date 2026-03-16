44253671fa2fa21643914845c2151c951175c048
Muayad67 <JwayMwa27@schueler.bwv-ac.de>
2026-03-16 10:11:38 +0100

    Tests

--- Rollenspiel/src/backend/FragenRepository.java
+++ Rollenspiel/src/backend/FragenRepository.java
@@ -8,9 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FragenRepository {
-    /*
-    Diese Klasse enthält Mock-Fragen zum Testen bis die Datenbank eingebunden ist
-     */
 
     public static List<Frage> alleFragen = null;
 
