4c5be39482d1d52f45e72e3f52259b576de08021
Muayad67 <JwayMwa27@schueler.bwv-ac.de>
2026-03-23 10:54:25 +0100

    Tests

--- Rollenspiel/src/tests/LueckentextGuiTest.java
+++ Rollenspiel/src/tests/LueckentextGuiTest.java
@@ -1,32 +1,12 @@
 package tests;
 
-import backend.Antwort;
-import backend.Frage;
-import backend.Spieler;
-import gui.LueckentextGUI;
 import gui.LueckentextPruefer;
-import gui.Startbildschirm;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.timeout;
+
 
 public class LueckentextGuiTest {
 
